08dbc3d651bcece5222e6650921e1346380eac5f
Van Andrew <53636492+vanandrewnguyen@users.noreply.github.com>
2024-02-02 16:58:21 +1100

    Create gasStation.cpp

---
+++ gasStation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int totalGas = 0, totalCost = 0, currGas = 0, startIndex = 0;
+        for (int i = 0; i < n; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+
+            currGas += gas[i] - cost[i];
+            if (currGas < 0) {
+                startIndex = i+1;
+                currGas = 0;
+            }
+        }
+
+        return (totalGas < totalCost) ? -1 : startIndex;
+    }
+};
+
+
